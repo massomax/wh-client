a61cd4c05ff5e9ca60a3a8bffc72923bcbf65a67
massomax <massocode@gmail.com>
2025-03-21 17:51:13 +0300

    1

--- src/pages/ProductPageSub.tsx
+++ src/pages/ProductPageSub.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useMemo } from 'react';
+import { useEffect, useState } from 'react';
 import { Product } from '../types/product';
 import { api } from '../api/client';
 
