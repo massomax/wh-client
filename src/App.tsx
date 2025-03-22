046de1777dcfc259b142b4ff16487533687d33a6
massomax <massocode@gmail.com>
2025-03-22 16:38:25 +0300

    replace employ || manager

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,27 @@
-import { createBrowserRouter, Navigate, RouterProvider, useLocation } from 'react-router-dom'
-import WarehousesPage from './pages/WarehousesPage'
-import ProductsPage from './pages/ProductPage'
-import ErrorPage from './components/ErrorPage'
-import LoginPage from './pages/LoginPage'
-import { JSX } from 'react'
+// App.tsx
+import { createBrowserRouter, Navigate, RouterProvider, useLocation } from 'react-router-dom';
+import WarehousesPage from './pages/WarehousesPage';
+import ProductsPage from './pages/ProductPage';
+import ErrorPage from './components/ErrorPage';
+import LoginPage from './pages/LoginPage';
+import ManagerDashboard from './pages/ManagerDashboard';
+import RegisterEmployee from './pages/RegisterEmployee';
+import WarehousePositions from './pages/WarehousePositions';
+import OrdersPage from './pages/OrdersPage';
+import WarehouseActions from './pages/WarehouseActions';
+import WarehouseReport from './pages/WarehouseReport';
+import ProtectedManagerRoute from './components/ProtectedManagerRoute';
+import { JSX } from 'react';
 import './telegramTheme.css';
 
 const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
-  const token = localStorage.getItem('authToken')
-  const location = useLocation()
+  const token = localStorage.getItem('authToken');
+  const location = useLocation();
 
   return token 
     ? children 
-    : <Navigate to="/login" replace state={{ from: location }} />
-}
+    : <Navigate to="/login" replace state={{ from: location }} />;
+};
 
 const router = createBrowserRouter([
   {
@@ -27,12 +35,42 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />
   },
   {
+    path: '/manager',
+    element: <ProtectedManagerRoute><ManagerDashboard /></ProtectedManagerRoute>,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: '/register-employee',
+    element: <ProtectedManagerRoute><RegisterEmployee /></ProtectedManagerRoute>,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: '/warehouse-positions',
+    element: <ProtectedManagerRoute><WarehousePositions /></ProtectedManagerRoute>,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: '/orders',
+    element: <ProtectedManagerRoute><OrdersPage /></ProtectedManagerRoute>,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: '/warehouse-actions',
+    element: <ProtectedManagerRoute><WarehouseActions /></ProtectedManagerRoute>,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: '/warehouse-report',
+    element: <ProtectedManagerRoute><WarehouseReport /></ProtectedManagerRoute>,
+    errorElement: <ErrorPage />
+  },
+  {
     path: '/login',
     element: <LoginPage />,
     errorElement: <ErrorPage />
   }
-])
+]);
 
 export default function App() {
-  return <RouterProvider router={router} />
+  return <RouterProvider router={router} />;
 }
